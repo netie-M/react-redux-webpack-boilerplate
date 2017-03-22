36c4393d8ab78e6962cf3fc834cc637bbaf86cb7
dreamcatchernick <bingu@quarkfinance.com>
2017-03-22 16:01:08 +0800

    init project

---
+++ src/app.js
@@ -0,0 +1,12 @@
+//css
+import './app.css';
+
+//vendor
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <h1>React is working!</h1>,
+    document.getElementById('root')
+);
